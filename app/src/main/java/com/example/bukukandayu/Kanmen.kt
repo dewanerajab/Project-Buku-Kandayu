4d7a45e3f09be7a826ee782a0647164fa9c5cde7
dewanerajab <82011991+dewanerajab@users.noreply.github.com>
2023-04-07 00:50:12 +0700

    fixing typo

--- app/src/main/java/com/example/bukukandayu/Kanmen.kt
+++ app/src/main/java/com/example/bukukandayu/Kanmen.kt
@@ -1,11 +1,10 @@
 package com.example.bukukandayu
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager.widget.ViewPager
-import android.content.Intent
 
 class Kanmen : AppCompatActivity(), View.OnClickListener {
 
